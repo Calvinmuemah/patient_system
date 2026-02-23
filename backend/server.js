f1757b7a887e46300bd9a9fd15089bb85033e42d
Calvinmuemah <kelvinmuemah855@gmail.com>
2026-02-23 19:19:12 +0300

    fix:update uis

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT;
 // connect to MongoDB
 connectDB();
 
+
+app.get('/status', (req, res) => {
+  res.send('Server is running');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
