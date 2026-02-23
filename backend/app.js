0d11f13a70debdaf0f054ffcc4ba3cb0d828c0c6
Calvinmuemah <kelvinmuemah855@gmail.com>
2026-02-23 19:40:01 +0300

    fix:update uis

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,11 @@ app.use("/api/patients", patientRoutes);
 app.use("/api/vitals", vitalsRoutes);
 app.use("/api/assessments", assessmentRoutes);
 
+// Default route for /api
+app.get('/api', (req, res) => {
+  res.send('API is running');
+});
+
 app.get('/', (req, res) => {
   res.send('Welcome to the backend server!');
 });
