d550bad27872ccf97560a200bda797e0ad4e69c5
Calvinmuemah <kelvinmuemah855@gmail.com>
2026-02-24 10:54:52 +0300

    fix:update mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const MONGO_URI = process.env.MONGO_URI;
-
+  // const MONGO_URI = process.env.MONGO_URI;
+  const MONGO_URI = "mongodb+srv://kelvinmuemah855:kelvinmuemah855@cluster0.wzdrk.mongodb.net/Patient_system"
   if (!MONGO_URI) {
     throw new Error("Please define MONGO_URI");
   }
