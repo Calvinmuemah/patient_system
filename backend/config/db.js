24817f36316de8ffe2760ae6e3e9bb318ef50dca
Calvinmuemah <kelvinmuemah855@gmail.com>
2026-02-24 10:47:59 +0300

    fix:update mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-    });
+  const MONGO_URI = process.env.MONGO_URI;
 
+  if (!MONGO_URI) {
+    throw new Error("Please define MONGO_URI");
+  }
+
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connection failed:");
-    console.error(error.message);
-    // Exit if DB fails
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 // connect to MongoDB
 connectDB();
-
+console.log("MONGO_URI:", process.env.MONGO_URI);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
